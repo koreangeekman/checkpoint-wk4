957f8c04684c522057e819d79c299a52632d6ab7
DJ Aaron <koreangeekman@gmail.com>
2023-10-13 14:57:42 -0600

    alignment tweaking

--- app/controllers/InspirationalController.js
+++ app/controllers/InspirationalController.js
@@ -3,23 +3,28 @@ import { inspirationalService } from "../services/InspirationalService.js";
 import { getFormData } from "../utils/FormHandler.js";
 import { setHTML } from "../utils/Writer.js";
 
+async function _getQuote() {
+  try {
+    const quoteObj = await inspirationalService.getQuote();
+    _drawHTML(quoteObj);
+  } catch (error) {
+    console.error('[InspirationalController] getQuote()', error);
+    Pop.error('[InspirationalController] getQuote()', error)
+  }
+}
+
 function _drawHTML(quoteObj) {
-  setHTML('quote', quoteObj.content)
+  const contentHTML = `
+  <p class="blueBlur px-2 quoteContent">${quoteObj.content}</p>
+  <p class="blueBlur px-2 quoteInfo">${quoteObj.author} • [${quoteObj.tags}]</p>
+  `
+  console.log(contentHTML);
+  setHTML('quote', contentHTML)
 }
 
 export class InspirationalController {
   constructor() {
-    this.getQuote();
-  }
-
-  async getQuote() {
-    try {
-      const quoteObj = await inspirationalService.getQuote();
-      _drawHTML(quoteObj);
-    } catch (error) {
-      console.error('[InspirationalController] getQuote()', error);
-      Pop.error('[InspirationalController] getQuote()', error)
-    }
+    _getQuote();
   }
 }
 
